316c6aa1edcfca81901d0503c1a6a5632ba09a14
Tsanain <9919103094@mail.jiit.ac.in>
2022-07-24 18:58:49 +0530

    decryptor final

--- lambda/decryptor.py
+++ lambda/decryptor.py
@@ -1,13 +1,14 @@
 from cryptography.fernet import Fernet
 import json
 
-encryptionKey = "epPGev7qqJg1yQlY0zTOrg-py3aUX3SslJOXAbNJ8t8="
+encryptionKey = "key here"
 
-encryptedData = b"gAAAAABi3UXkTQ29lNeO7WYR2B5dYMgSJtVTkPFznfhJDOjDLSIMY6w3GnYmnHtSBXQKBimqoWd7Oat5UGn7Q9Ihi-kSLpoxTRRU87CLAmnKvW4Q72N9J2maUGbMQZDp1PYLng-vBrs5ugJKIGqk-6oaEDFQax_h5_fMIB3qHJyeYH-8XjPM1es="
+encryptedData = b"data here"
 
 cryptor = Fernet(encryptionKey)
 
 decryptedData = cryptor.decrypt(encryptedData)
+
 decryptedData = json.loads(decryptedData)
 
 print(decryptedData)
